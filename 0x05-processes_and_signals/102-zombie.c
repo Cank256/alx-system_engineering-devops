496d26fbbb7d470580a89a1474cdff3d83cade10
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-03 09:38:48 +0300

    zombie update

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -2,28 +2,41 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * main - Entry point for a program to create zombie processes.
+ *
+ * Description:
+ * This program demonstrates the creation of zombie processes using the fork()
+ * system call. It creates 5 child processes, each displaying a message with
+ * its PID and then exiting, becoming a zombie. The parent process sleeps for
+ * 10 seconds to allow the zombies to accumulate.
+ *
+ * Return:
+ * Always returns 0.
+ */
 int main(void)
 {
-    pid_t child_pid;
-    int i;
+pid_t child_pid;
+int i;
 
-    for (i = 0; i < 5; i++) {
-        child_pid = fork();
+for (i = 0; i < 5; i++)
+{
+child_pid = fork();
 
-        if (child_pid == -1) {
-            perror("Fork failed");
-            exit(1);
-        }
+if (child_pid == -1)
+{
+perror("Fork failed");
+exit(1);
+}
 
-        if (child_pid == 0) {
-            // Child process
-            printf("Zombie process created, PID: %d\n", getpid());
-            exit(0);
-        }
-    }
+if (child_pid == 0)
+{
+printf("Zombie process created, PID: %d\n", getpid());
+exit(0);
+}
+}
 
-    // Parent process
-    sleep(10); // Sleep for 10 seconds to allow the zombies to accumulate
+sleep(10);
 
-    return (0);
+return (0);
 }
