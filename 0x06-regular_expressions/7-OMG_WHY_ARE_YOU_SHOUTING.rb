7e577979a4f87c700242ec3f670ea0c5f0539cc4
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-10-04 09:32:34 +0300

    capitals

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+def match_string(arg)
+    regex = /[A-Z]+/
+    matched_text = arg.scan(regex).join
+    puts matched_text
+end
+
+# Get the argument from the command line
+argument = ARGV[0]
+
+# Call the match_string method
+match_string(argument)
+
