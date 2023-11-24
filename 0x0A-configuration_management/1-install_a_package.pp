223fd4477f30571d4129b88c1aef36948c1fcecb
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-11-24 16:03:42 +0300

    install flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,8 +6,8 @@ package { 'python3-pip':
 }
 
 exec { 'install_flask':
+  require => Package['python3-pip'],
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => '/usr/bin',
   creates => '/usr/local/lib/python3.8/dist-packages/flask',
-  require => Package['python3-pip'],
 }
