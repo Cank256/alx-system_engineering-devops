ea2f0ce29b3a0376bc0e0733e1eb54a5f1bdb1cb
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-11-27 08:33:21 +0300

    ssh config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# ssh_config.pp
+
+file { '~/.ssh/config':
+  ensure => present,
+  content => '
+    Host school
+      HostName 18.207.3.75
+      User ubuntu
+      IdentityFile ~/.ssh/school
+      PreferredAuthentications publickey
+      PasswordAuthentication no
+  ',
+  owner => 'ubuntu',
+  group => 'ubuntu',
+  mode  => '0600',
+}
