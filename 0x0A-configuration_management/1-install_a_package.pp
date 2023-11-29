37ce6d0d2a6418b64669cf66df7fc609086d9baf
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-11-29 08:13:09 +0300

    puppetier

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,10 +5,15 @@ package { 'python3-pip':
   ensure => 'installed',
 }
 
-exec { 'install_flask':
+exec { 'install_werkzeug':
   require => Package['python3-pip'],
+  command => 'pip3 install werkzeug==2.0.2',
+  path    => ['/usr/bin/'],
+}
+
+exec { 'install_flask':
+  require => [Package['python3-pip'], Exec['install_werkzeug']],
   command => 'pip3 install flask==2.1.0',
   path    => ['/usr/bin/'],
-  unless  => '/usr/bin/test -f \
-  /usr/local/lib/python3.4/dist-packages/flask/app.py'
+  unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
 }
