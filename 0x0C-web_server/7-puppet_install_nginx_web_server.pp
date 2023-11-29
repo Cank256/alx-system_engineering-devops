b05958952674e527985d3d50cef319ddf20b0bce
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-11-29 12:19:37 +0300

    check nginx config with puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,21 @@
-# 7-puppet_install_nginx_web_server.pp
+# puppet_redirect_and_error_handling.pp
 
-# Install Nginx package
+# Update the package list
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
+}
+
+# Install nginx
 package { 'nginx':
   ensure => 'installed',
+  require => Exec['apt-update'],
 }
 
-# Ensure Nginx service is running
+# Ensure nginx is running
 service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
+  ensure => 'running',
+  enable => true,
   require => Package['nginx'],
-}
-
-# Template for Nginx default configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  notify  => Service['nginx'],
 }
 
 # Create a custom 404 page
@@ -24,4 +23,21 @@ file { '/var/www/html/404.html':
   ensure  => file,
   content => 'Ceci n\'est pas une page',
   notify  => Service['nginx'],
+}
+
+# Restart nginx without using systemctl
+exec { 'nginx-restart':
+  command => '/usr/sbin/service nginx restart',
+}
+
+# Verify nginx configuration
+exec { 'nginx-verify':
+  command => '/usr/sbin/nginx -t',
+  require => Exec['nginx-restart'],
+}
+
+# Reload nginx configuration
+exec { 'nginx-reload':
+  command => '/usr/sbin/service nginx reload',
+  require => Exec['nginx-verify'],
 }
