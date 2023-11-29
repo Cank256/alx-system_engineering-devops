592d6ba308556e4bf78aed9b0b87d872888a01ca
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-11-29 08:45:19 +0300

    puppetier kill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -2,13 +2,6 @@
 # process named 'killmenow' using pkill
 
 exec { 'kill_killmenow':
-  command => 'pkill -TERM killmenow',
-  onlyif  => 'pgrep killmenow',
-  timeout => 10, # Adjust the timeout value as needed
-  notify  => Exec['check_process_status'],
-}
-
-exec { 'check_process_status':
-  command => 'pgrep killmenow',
-  unless  => 'pgrep killmenow',
+  command     => 'pkill killmenow',
+  refreshonly => true,
 }
