e0b444b4db3d86179ec920ed5c5f52660126c1a0
Caleb Nkunze <nkunzecaleb@gmail.com>
2023-12-04 12:45:45 +0300

    load balance

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -7,18 +7,19 @@ class { 'nginx':
 
 # Define custom HTTP header
 $custom_header_name = 'X-Served-By'
-$custom_header_value = $::hostname
+$custom_header_value = $facts['hostname']
 
 # Configure Nginx to add custom header
 file { '/etc/nginx/sites-available/default':
   ensure  => file,
-  content => "server {\n\tlocation / {\n\t\tadd_header $custom_header_name $custom_header_value;\n\t\t# Other Nginx configurations...\n\t}\n}",
+  content => "server {\n\tlocation / {\n\t\tadd_header ${custom_header_name} \
+  ${custom_header_value};\n\t\t# Other Nginx configurations...\n\t}\n}",
   notify  => Service['nginx'],
 }
 
 # Restart Nginx for changes to take effect
 service { 'nginx':
-  ensure => 'running',
-  enable => true,
+  ensure  => 'running',
+  enable  => true,
   require => File['/etc/nginx/sites-available/default'],
 }
