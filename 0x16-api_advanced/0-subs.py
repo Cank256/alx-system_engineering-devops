5843188ebf8f57ebe109f9c5aae62f7ee80e0b20
Caleb Nkunze <nkunzecaleb@gmail.com>
2024-01-16 13:14:48 +0300

    advanced api update checker errors

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-Module to query the Reddit API and return the number of subscribers for a subreddit.
+Module to query the Reddit API and return the number of subscribers for
+a subreddit.
 """
 import requests
+
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'python3:alx-system_engineering-devops:v1.0.0 (by /u/calebnkunze)'}
+    headers = {
+        'User-Agent': 'python3:alx-system_engineering-devops:v1.0.0 \
+            (by /u/calebnkunze)'
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-Module to query the Reddit API and print the titles of the top 10 hot posts of a subreddit.
+Module to query the Reddit API and print the titles of the top 10 hot posts of
+a subreddit.
 """
 import requests
+
 
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts of a subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'python3:alx-system_engineering-devops:v1.0.0 (by /u/calebnkunze)'}
+    headers = {
+        'User-Agent': 'python3:alx-system_engineering-devops:v1.0.0 \
+            (by /u/calebnkunze)'
+        }
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
